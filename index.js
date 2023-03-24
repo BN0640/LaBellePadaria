63ec633ece0a36bf22709b3dc7acd53b9115ba03
davi nardi <davinardi64@gmail.com>
2023-03-24 15:28:49 -0300

    0.0.4 commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-function uploadFile(image){
+function uploadFile(path){
     var fileInput = document.getElementById('file-input');
     fileInput.addEventListener("change", function() {
         var file = fileInput.files[0];
         var reader = new FileReader();
         reader.addEventListener("load", function(){
-            image.src = reader.result;
+            path.src = reader.result;
         })
         reader.readAsDataURL(file);
     });
@@ -13,19 +13,6 @@ function uploadFile(image){
     fileInput.dispatchEvent(event);
 };
 
-function refreshText(){
-    titleText = document.getElementById('title-input').value
-    document.getElementById("title-output").innerText = titleText
-
-    descriptionText = document.getElementById('description-input').value
-    document.getElementById("description-output").innerText = descriptionText
-
-    priceText = document.getElementById('price-input').value
-    document.getElementById("price-output").innerText = priceText
-    
-    availableText = document.getElementById('available-input').value
-    document.getElementById("available-output").innerText = availableText
-}
 function refreshTitleText(){
     titleText = document.getElementById('title-input').value
     document.getElementById("title-output").innerText = titleText
@@ -42,16 +29,23 @@ function refreshAvailableText(){
     availableText = document.getElementById('available-input').value
     document.getElementById("available-output").innerText = availableText
 }
-function refreshColor(){
+function refreshColorTitle(){
     color_title = document.getElementById('color-picker-title').value
     document.getElementById("title-output").style.color = color_title
-
+}
+function refreshColorDescription(){
     color_description = document.getElementById('color-picker-description').value
     document.getElementById("description-output").style.color = color_description
-
+}
+function refreshColorPrice(){
     color_price = document.getElementById('color-picker-price').value
     document.getElementById("price-output").style.color = color_price
-    
+}
+function refreshColorAvailable(){
     color_available = document.getElementById('color-picker-available').value
     document.getElementById("available-output").style.color = color_available
+}
+function refreshColorBg(){
+    color_bg = document.getElementById('color-picker-bg').value
+    document.getElementById("pv-description").style.backgroundColor = color_bg  
 }
